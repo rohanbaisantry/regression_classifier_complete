8f81f68dc6bfff82fc11febacfa58addd51c78cd
Rohan Baisantry <rohanbaisantry7@gmail.com>
2018-03-01 16:26:25 +0530

    selects classification or regression - not always accurate.

--- algorithm_selection.py
+++ algorithm_selection.py
@@ -1,26 +1,15 @@
 
-# this program is choose what machine learning algorithm to use.
-# note : it is only for supervised learning.
-# classification v/s. regression
+# Program to check if the problem is a Classification or Regression problem
+# using the type of target function in the SK-Learn library
+# http://scikit-learn.org/dev/modules/generated/sklearn.utils.multiclass.type_of_target.html
 
-"""
-Idea : 
-run though the last column of the data ( the output ) and decide whether they are class labels or if they are continuous values for a regrssion.
-return 1 for classification problem and 2 for regression problem.
-"""
-def algorithmcheck(y_train):
-    count = {}
-    output = set()
-    for x in y_train:
-        output.add(x)
-    if len(y_train)==len(output):
-    	return 2
-    for x in output:
-	    count[x]=0;
-    for i in len(y_train):
-	    if y_train[i].isalpha():
-		    return 1
-	    else:
-		    count[y_train[i]]+=1
-    if len()
+from sklearn.utils.multiclass import type_of_target
 
+def algorithm_select(y_train):
+	output = type_of_target(y_train)
+	if output == 'unknown':
+		return 0
+	if output == 'continuous':
+		return 1
+	else:
+		return 2
