c2d361d2d305a85c5897df96bbf5fbc371fdcb48
Rohan Baisantry <rohanbaisantry7@gmail.com>
2018-03-01 16:25:22 +0530

    accuracy update to give confusion matrix and accuracy %

--- accuracy.py
+++ accuracy.py
@@ -1,6 +1,5 @@
 
 #program to check the accuracy of the algorithm
-# just checks for % of correct predictions as the accracy
 
 def accuracy_metric(actual, predicted):
 	correct = 0
@@ -8,3 +7,11 @@ def accuracy_metric(actual, predicted):
 		if actual[i] == predicted[i]:
 			correct += 1
 	return correct / float(len(actual)) * 100.0
+
+def conf_matrix_metric(expected, predicted, n_classes):
+    m = [[0] * n_classes for i in range(n_classes)]
+    for pred, exp in zip(predicted, expected):
+        m[pred][exp] += 1
+    t = sum(sum(l) for l in conf_matrix)
+    acurracy= sum(conf_matrix[i][i] for i in range(len(conf_matrix)))/t
+    return [m, acurracy]
